fb3f3ea070b4e9600098ff57d9b3d2b7ce45ca7c
KWJ30 <gidtjrwns30@naver.com>
2024-12-24 18:09:18 +0900

    241224

---
+++ src/main/java/com/mbc/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.mbc.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.info("----------SecurityFilterChain 진입 체크----------");
+
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/css/**","/js/**","/img/**").permitAll()
+                        .requestMatchers("/","/members/**","/item/**","/images/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN") // admin 관련 URL 보호
+                        .anyRequest().authenticated() // 그 외 모든 요청 로그인 요구
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/members/login")
+                        .defaultSuccessUrl("/")
+                        .usernameParameter("email")
+                        .failureUrl("/members/login/error")
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                        .logoutSuccessUrl("/")
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
