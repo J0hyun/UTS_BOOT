541a0a220dccaea129f062b5dd9460e3f9b44b03
KWJ30 <gidtjrwns30@naver.com>
2024-12-31 15:20:00 +0900

    241231 second

--- src/main/java/com/mbc/repository/CategoryRepository.java
+++ src/main/java/com/mbc/repository/CategoryRepository.java
@@ -4,6 +4,7 @@ import com.mbc.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
@@ -12,4 +13,5 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     // 특정 부모 카테고리의 하위 카테고리 리스트를 찾는 쿼리
     List<Category> findByParent_Id(Long parentCategoryId);
+
 }
