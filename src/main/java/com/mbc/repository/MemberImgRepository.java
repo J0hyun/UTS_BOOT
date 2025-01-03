5069816edb3c5c9b3d3adf0e4e89502d8184ef98
KWJ30 <gidtjrwns30@naver.com>
2025-01-03 12:21:49 +0900

    250103 second

---
+++ src/main/java/com/mbc/repository/MemberImgRepository.java
@@ -0,0 +1,7 @@
+package com.mbc.repository;
+
+import com.mbc.entity.MemberImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberImgRepository extends JpaRepository<MemberImg, Long> {
+}
